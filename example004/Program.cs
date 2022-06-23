83a088fe7cce0df4504694c72546fd4be0fc2838
Katarina <novikkatarina@gmail.com>
2022-06-23 20:08:29 +0400

    example004

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,3 +1,5 @@
-﻿double NumberA = 12;
-double NumberB=5;
+﻿double NumberA = new Random ().Next(1,10);
+Console.WriteLine (NumberA);
+double NumberB= new Random ().Next(1,10);
+Console.WriteLine (NumberB);
 Console.WriteLine (NumberA/ NumberB);
